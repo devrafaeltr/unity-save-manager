18b965daa1ea06ec71d459ddc9318848eec0af0c
Rafael T. Ribeiro <rafaeltrpessoal@gmail.com>
2020-07-19 16:25:57 -0300

    Remove JSON conversion

--- Assets/devrafael/Scripts/Manager_Save.cs
+++ Assets/devrafael/Scripts/Manager_Save.cs
@@ -15,30 +15,29 @@ public class Manager_Save
         T dataToLoad = new T();
         bool success = false;
         string path = GetPath(saveInfo);
+        Debug.Log(path);
 
         if (File.Exists(path))
         {
             BinaryFormatter binaryFormatter = new BinaryFormatter();
-            FileStream fileToOpen = File.Open(path, FileMode.Open);
+            FileStream fileToLoad = File.Open(path, FileMode.Open);
 
-            string encryptedJsonData = (string)binaryFormatter.Deserialize(fileToOpen);
-            string decyptedJsonData = SimpleEncryption.Decrypt(encryptedJsonData);
+            dataToLoad = (T)binaryFormatter.Deserialize(fileToLoad);
 
-            dataToLoad = FromJson<T>(decyptedJsonData);
-            fileToOpen.Close();
+            fileToLoad.Close();
 
             success = true;
 
-            #if UNITY_EDITOR
-            Debug.Log($"Loaded JSON at {path} \nEncrypted as: {encryptedJsonData}\nDecrypted to {typeof(T)}: {decyptedJsonData}");
-            #endif
+#if UNITY_EDITOR
+            Debug.Log($"Loaded {typeof(T)} from {path}.");
+#endif
         }
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         else
         {
             Debug.Log($"No file found. Returning new {typeof(T)}");
         }
-        #endif
+#endif
 
         resultCallback?.Invoke(success);
 
@@ -53,27 +52,23 @@ public class Manager_Save
         if (data != null)
         {
             BinaryFormatter binaryFormatter = new BinaryFormatter();
-            FileStream fileToCreate = File.Open(path, FileMode.OpenOrCreate);
+            FileStream savedFile = File.Open(path, FileMode.OpenOrCreate);
 
-            string decyptedJsonData = DataToJson((T)data);
-            string encryptedJsonData = SimpleEncryption.Encrypt(decyptedJsonData);
-
-
-            binaryFormatter.Serialize(fileToCreate, encryptedJsonData);
-            fileToCreate.Close();
+            binaryFormatter.Serialize(savedFile, data);
+            savedFile.Close();
 
             success = true;
 
-        #if UNITY_EDITOR
-            Debug.Log($"Saved {typeof(T)} at {path}\nas JSON: {decyptedJsonData}\nEncrypted to: {encryptedJsonData}");
-        #endif
+#if UNITY_EDITOR
+            Debug.Log($"Saved {typeof(T)} at {path}.");
+#endif
         }
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         else
         {
             Debug.LogError($"data param is null. Nothing to save.");
         }
-        #endif
+#endif
 
         resultCallback?.Invoke(success);
     }
@@ -84,59 +79,24 @@ public class Manager_Save
         string tempPath = GetPath(saveInfo);
         if (File.Exists(tempPath))
         {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             Debug.Log($"Deleted {saveInfo.name}{saveInfo.format} at {Application.persistentDataPath}");
-            #endif
+#endif
             File.Delete(tempPath);
             success = true;
         }
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         else
         {
             Debug.Log($"No file named {saveInfo.name}{saveInfo.format} found at {Application.persistentDataPath}");
         }
-        #endif
+#endif
 
         resultCallback?.Invoke(success);
     }
 
     private static string GetPath((string name, string format) saveData)
     {
-        return $"{Application.persistentDataPath}{saveData.name}{saveData.format}";
-    }
-
-    private static string DataToJson(object data)
-    {
-        if (data != null)
-        {
-            #if UNITY_EDITOR
-            return JsonUtility.ToJson(data, true);
-            #else
-            return JsonUtility.ToJson(data);
-            #endif
-
-        }
-        else
-        {
-            #if UNITY_EDITOR
-            Debug.LogError($"data param is nul. Nothing to convert. Returning an empty string.");
-            #endif
-            return "";
-        }
-    }
-
-    private static T FromJson<T>(string jsonData)
-    {
-        if (!string.IsNullOrEmpty(jsonData))
-        {
-            return JsonUtility.FromJson<T>(jsonData);
-        }
-        else
-        {
-            #if UNITY_EDITOR
-            Debug.LogError($"jsonData param is empty. Nothing to convert. Returning a default value for {typeof(T)}.");
-            #endif
-            return default;
-        }
+        return $"{Application.persistentDataPath}/{saveData.name}{saveData.format}";
     }
 }
